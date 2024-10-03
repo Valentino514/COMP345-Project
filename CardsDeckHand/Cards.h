768ab5d821e160a7687afc7c3342f83399509b6f
mohammad adas <133288252+Mohammad-Adas@users.noreply.github.com>
2024-10-02 21:23:09 -0400

    Update Cards.h

--- CardsDeckHand/Cards.h
+++ CardsDeckHand/Cards.h
@@ -1,24 +1,52 @@
-#ifndef CARDS_H_
-#define CARDS_H_
+#ifndef CARDS_H
+#define CARDS_H
 
-class Deck{
+#include <iostream>
+#include <vector>
 
-    public:
+// Card class definition
+class Card {
+public:
+    // Enumeration of card types
+    enum CardType { Bomb, Reinforcement, Blockade, Airlift, Diplomacy };
 
-    void draw();
+    // Constructor to initialize the card with a type
+    Card(CardType type);
 
-    private:
-
-
-};
-
-class Card{
-
+    // Method to play the card (to be expanded later in Assignment 2)
     void play();
+
+    // Get the type of the card
+    CardType getType() const;
+
+    // Return the name of the card type as a string
+    std::string getCardTypeName() const;
+
+private:
+    CardType type;  // Holds the type of the card
 };
 
-class hand{
-    
+// Deck class definition
+class Deck {
+public:
+    Deck();  // Constructor to initialize the deck with cards
+    Card* draw();  // Draw a card from the deck
+    void returnCard(Card* card);  // Return a played card back to the deck
+
+private:
+    std::vector<Card*> cards;  // Collection of cards in the deck
+    void initializeDeck();  // Fill the deck with cards
+};
+
+// Hand class definition
+class Hand {
+public:
+    Hand();  // Constructor to initialize an empty hand
+    void addCard(Card* card);  // Add a card to the hand
+    void playAll(Deck* deck);  // Play all cards in the hand, returning them to the deck
+
+private:
+    std::vector<Card*> handCards;  // Collection of cards in the player's hand
 };
 
 #endif
