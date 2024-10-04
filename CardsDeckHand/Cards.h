9645d18821a221359aa49df019e27360311dcf96
mohammad adas <133288252+Mohammad-Adas@users.noreply.github.com>
2024-10-04 00:05:12 -0400

    Update Cards.h

--- CardsDeckHand/Cards.h
+++ CardsDeckHand/Cards.h
@@ -13,6 +13,9 @@ public:
     // Constructor to initialize the card with a type
     Card(CardType* type);
 
+    // Destructor to clean up dynamically allocated memory
+    ~Card();
+
     // Method to play the card (to be expanded later in Assignment 2)
     void play();
 
@@ -30,11 +33,12 @@ private:
 class Deck {
 public:
     Deck();  // Constructor to initialize the deck with cards
+    ~Deck();  // Destructor to clean up dynamically allocated memory
     Card* draw();  // Draw a card from the deck
     void returnCard(Card* card);  // Return a played card back to the deck
 
 private:
-    std::vector<Card*> cards;  // Collection of cards in the deck
+    std::vector<Card*>* cards;  // Collection of cards in the deck (as a pointer)
     void initializeDeck();  // Fill the deck with cards
 };
 
@@ -42,11 +46,12 @@ private:
 class Hand {
 public:
     Hand();  // Constructor to initialize an empty hand
+    ~Hand();  // Destructor to clean up dynamically allocated memory
     void addCard(Card* card);  // Add a card to the hand
     void playAll(Deck* deck);  // Play all cards in the hand, returning them to the deck
 
 private:
-    std::vector<Card*> handCards;  // Collection of cards in the player's hand
+    std::vector<Card*>* handCards;  // Collection of cards in the player's hand (as a pointer)
 };
 
 #endif
